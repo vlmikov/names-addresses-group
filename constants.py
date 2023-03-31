2e6710ddd2777ff0c7540194cf815e13fcc5e8d6
Vladimir Mikov <vladimirjmikov@gmail.com>
2023-03-31 12:19:54 +0700

    Revert "Revert "Code clean, refactor, add fuzzy compare functionality""
    This reverts commit ec93c6fd152bbdf891ffa9eaeba14b04939c49be.

--- constants.py
+++ constants.py
@@ -1,2 +1,53 @@
-API_KEY = "6b1e128dxxxxxxxxxxxxxxxxxxxxxxxx"
-expected_header = ['Name', 'Address']
+API_KEY = "6b1e128d93e14a8d8ba097464323d218"
+expected_header = ["Name", "Address"]
+
+
+verbose = True
+similarity_score_threshold = 50
+data_street_main = {
+    "data": [
+        ["street ", "ul.", "str.", "ul ", "str ", "ulitsa"],
+        ["avenue ", "ave.", "ave "],
+        ["boulevard ", "bul." "blvd.", "boulevard  ", "bulevard "],
+        ["road ", "rd.", "rd "],
+        ["lane ", "ln.", "ln "],
+        ["drive ", "dr.", "dr "],
+        ["place ", "pl." "pl "],
+        ["court ", "ct.", "ct "],
+        ["terrace ", "ter.", "ter "],
+        ["parkway ", "pkwy.", "pkwy "],
+    ]
+}
+
+# Define the Cyrillic to Latin transliteration dictionary
+translit_dict = {
+    "а": "a",
+    "б": "b",
+    "в": "v",
+    "г": "g",
+    "д": "d",
+    "е": "e",
+    "ж": "zh",
+    "з": "z",
+    "и": "i",
+    "й": "y",
+    "к": "k",
+    "л": "l",
+    "м": "m",
+    "н": "n",
+    "о": "o",
+    "п": "p",
+    "р": "r",
+    "с": "s",
+    "т": "t",
+    "у": "u",
+    "ф": "f",
+    "х": "h",
+    "ц": "ts",
+    "ч": "ch",
+    "ш": "sh",
+    "щ": "sht",
+    "ъ": "u",
+    "ю": "yu",
+    "я": "a",
+}
